b92187dbcae254c214d18a967ecd374100b78733
CODE-INAYAT <134628417+CODE-INAYAT@users.noreply.github.com>
2024-10-21 01:01:27 +0530

    Formated

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-        import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
-
-        // API
-        const firebaseConfig = {
-            apiKey: "AIzaSyBQsKFqSksq-mJHWiXZJBq7uQXI1VscfNI",
-            authDomain: "formdatabase-9286d.firebaseapp.com",
-            projectId: "formdatabase-9286d",
-            storageBucket: "formdatabase-9286d.appspot.com",
-            messagingSenderId: "79926329148",
-            appId: "1:79926329148:web:33bb11f4d0f3f353ac6839"
-        };
-
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const db = getDatabase(app);
-        document.getElementById("submit").addEventListener('click', function (e) {
-            e.preventDefault();
-            document.getElementById("loading").style.display = 'flex';
-            document.getElementById("details-form").style.display = 'none';
-
-            // Get user details
-            const userName = document.getElementById("name").value;
-            const userEmail = document.getElementById("email").value;
-            const userBranch = document.getElementById("branch").value;
-            const userYear = document.getElementById("year").value;
-
-            set(ref(db, 'user/' + userName), {
-                username: userName,
-                email: userEmail,
-                branch: userBranch,
-                year: userYear
-            });
-
-            setTimeout(function () {
-                document.getElementById("loading").style.display = 'none';
-                document.getElementById("success").style.display = 'flex';
-                document.getElementById("displayName").textContent = userName;
-            }, 5000);
-        });
-
-        document.querySelector('#success a').addEventListener('click', function (e) {
-            e.preventDefault();
-            document.getElementById("success").style.display = 'none';
-            document.getElementById("details-form").style.display = 'flex';
-            document.getElementById("accessForm").reset();
-        });
-
 // Security measures
 document.addEventListener('keydown', e => {
     if (e.keyCode === 123 ||
@@ -54,13 +6,6 @@ document.addEventListener('keydown', e => {
         (e.ctrlKey && e.keyCode === 85) ||
         (e.metaKey && e.altKey && e.keyCode === 85)) {
         e.preventDefault();
-    }
-});
-
-document.addEventListener('keyup', e => {
-    if (e.key === 'PrintScreen') {
-        navigator.clipboard.writeText('');
-        alert('Screenshots are not allowed on this page.');
     }
 });
 
